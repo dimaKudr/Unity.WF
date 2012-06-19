5c90a76320f6203bb8e1e9b458dbfbd043936381
Dmytro Kudryavtsev <dimaKudr@gmail.com>
2012-06-19 08:26:01 +0300

    Samples were updated to demonstrate both implemented approaches to inject dependencies into WF services.

---
+++ Unity.WF.SampleService/SamplePullFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Practices.Unity;
+using Unity.WF.SampleActivities;
+
+namespace Unity.WF.SampleService
+{
+    public class SamplePullFactory : UnityServiceHostFactory
+    {
+        protected override void ConfigureContainer(IUnityContainer container)
+        {
+            container.RegisterType<ICalcService, CalcService>();
+        }
+
+        protected override InjectionTypes ConfigureInjectionType()
+        {
+            return InjectionTypes.Pull;
+        }
+    }
+}
--- Unity.WF.SampleService/SamplePushFactory.cs
+++ Unity.WF.SampleService/SamplePushFactory.cs
@@ -3,11 +3,16 @@ using Unity.WF.SampleActivities;
 
 namespace Unity.WF.SampleService
 {
-    public class SampleFactory : UnityServiceHostFactory
+    public class SamplePushFactory : UnityServiceHostFactory
     {
         protected override void ConfigureContainer(IUnityContainer container)
         {
             container.RegisterType<ICalcService, CalcService>();
+        }
+
+        protected override InjectionTypes ConfigureInjectionType()
+        {
+            return InjectionTypes.Push;
         }
     }
 }
